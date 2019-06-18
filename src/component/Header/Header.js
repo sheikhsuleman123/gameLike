e1a36851ea06897b65161b30599cf23f4eb69fd9
sheikhsuleman123 <mssheikh333@gmail.com>
2019-06-18 19:29:27 +0500

    apply the dropdown menu and Header directory

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,23 +5,39 @@ class Header extends Component {
   render() {
     return (
       
-      <div >
-
-   <nav class="navbar navbar-expand-lg navbar-light bg-light ">  
+      <div className="header">
+   <nav class="navbar navbar-expand-lg ">  
        
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  
+  <div>
+  <img src="" class="img-fluid img-thumbnail" alt="Smiley face" height="42" width="42" />
        <span className="logo">gamelike</span>
-           
+       </div>    
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Guide <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Players</a>
+        <div class="btn-group">
+  <button type="button" class="btn nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Guide
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Game Setting</a>
+  </div>
+    </div>
+    <div class="btn-group">
+  <button type="button" class="btn nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Players
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Game Setting</a>
+    <a class="dropdown-item" href="#">Game Setting</a>
+    <a class="dropdown-item" href="#">Game Setting</a>
+    <a class="dropdown-item" href="#">Game Setting</a>
+
+  </div>
+    </div>
       </li>
     </ul>
     
